693c56d32a9475497af62a92064d8e9cbea85461
Stefan <iosifs617@gmail.com>
2023-05-31 16:59:57 +0300

    main

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -127,6 +127,7 @@ export function AuthProvider({ children }) {
   const { t } = useTranslation()
   const [currentUser, setCurrentUser] = useState();
   const [loading, setLoading] = useState(true);
+  console.log(process.env.REACT_APP_SERVER)
   const server = process.env.REACT_APP_SERVER
   const [admin, setAdmin] = useState(false)
   const [ban, setBan] = useState()
